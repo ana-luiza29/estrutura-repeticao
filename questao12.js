e933b37654b22064d85aa57f26f360faa304ac94
ana-luiza29 <freitas.analuiza29@gmail.com>
2024-04-03 10:51:43 -0400

    finalizada a questao 12

---
+++ questao12.js
@@ -0,0 +1,13 @@
+/*
+  
+ 
+12. Questão: Escreva um código que imprima os números pares de 2 a 10.  
+
+*/
+ repostas: 
+
+ console.log("Questão 12:");
+ for (let i = 2; i <= 10; i += 2) {
+     console.log(i);
+ }
+
